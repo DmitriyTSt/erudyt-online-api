85d39af8f92ff0ca72a6611bb8f6597894cee35c
Dmitriy Tomilov <tomilov.d.a@mail.ru>
2022-03-05 00:06:27 +0400

    add temp result entity

---
+++ src/main/kotlin/ru/erudyt/online/entity/api/TempResultEntity.kt
@@ -0,0 +1,25 @@
+package ru.erudyt.online.entity.api
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "temp_result")
+class TempResultEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long = 0,
+    val isAnon: Boolean,
+    val userId: Long,
+    val code: String,
+    val place: Int,
+    val result: Int,
+    @Column(name = "max_ball") val maxBall: Int,
+    val answers: String,
+    val sequence: String,
+    val correct: String,
+)
