11c2b0ec94e7e2091b263c43e68cca7ad669cada
Dmitriy Tomilov <tomilov.d.a@mail.ru>
2022-04-21 09:55:13 +0400

    add head to page

--- src/main/kotlin/ru/erudyt/online/service/WebPageService.kt
+++ src/main/kotlin/ru/erudyt/online/service/WebPageService.kt
@@ -51,19 +51,20 @@ class WebPageService @Autowired constructor(
             path
         }
         val doc = Jsoup.connect("${domainSettings.baseUrl}${fixedPath}").get()
-        val container = doc.body().select("#container")
-        val title1 = container.select("h1").takeIf { it.isNotEmpty() }?.first()?.html()
+        doc.body().select("header").first().remove()
+        doc.body().select("footer").first().remove()
+        val title1 = doc.select("h1").takeIf { it.isNotEmpty() }?.first()?.html()
         val title2 = if (title1 == null) {
-            container.select("h2").takeIf { it.isNotEmpty() }?.first()?.html()
+            doc.select("h2").takeIf { it.isNotEmpty() }?.first()?.html()
         } else {
             null
         }
         if (title1 != null) {
-            container.select("h1").first().remove()
+            doc.select("h1").first().remove()
         }
         if (title2 != null) {
-            container.select("h2").first().remove()
+            doc.select("h2").first().remove()
         }
-        return WebPage((title1 ?: title2).orEmpty(), container.html())
+        return WebPage((title1 ?: title2).orEmpty(), doc.html())
     }
 }
