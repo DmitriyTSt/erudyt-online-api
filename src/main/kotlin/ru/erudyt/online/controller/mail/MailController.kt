ff4d0b79a82fcd55f82fce3b25b1c6ffa28f529c
Dmitriy Tomilov <tomilov.d.a@mail.ru>
2022-06-07 19:20:58 +0400

    add test mail controller, add server port property

---
+++ src/main/kotlin/ru/erudyt/online/controller/mail/MailController.kt
@@ -0,0 +1,28 @@
+package ru.erudyt.online.controller.mail
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import ru.erudyt.online.config.EnvSettings
+import ru.erudyt.online.dto.enums.ApiError
+import ru.erudyt.online.dto.enums.getException
+import ru.erudyt.online.dto.request.TestMailRequest
+import ru.erudyt.online.service.MailService
+
+@RestController
+@RequestMapping("/api/", produces = [MediaType.APPLICATION_JSON_VALUE])
+class MailController @Autowired constructor(
+    private val mailService: MailService,
+    private val envSettings: EnvSettings,
+) {
+
+    @GetMapping("test-mail")
+    fun main(request: TestMailRequest): ResponseEntity<Unit> {
+        if (envSettings.env != EnvSettings.DEV) throw ApiError.NOT_AVAILABLE_BY_ENV.getException()
+
+        return ResponseEntity.ok(mailService.sendEmail(request.to, request.subject, request.text))
+    }
+}
