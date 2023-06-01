fdbdb03a8ebe2de4758996ba93aa0c9c8a6f7b97
Dmitriy Tomilov <tomilov.d.a@mail.ru>
2023-06-01 23:49:50 +0400

    refactoring security config

--- src/main/kotlin/ru/erudyt/online/config/SecurityConfig.kt
+++ src/main/kotlin/ru/erudyt/online/config/SecurityConfig.kt
@@ -1,23 +1,23 @@
 package ru.erudyt.online.config
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import ru.erudyt.online.config.jwt.JwtAuthenticationEntryPoint
 import ru.erudyt.online.config.jwt.JwtAuthorizationFilter
 
 @Configuration
-@EnableWebSecurity
 class SecurityConfig @Autowired constructor(
     private val jwtAuthorizationFilter: JwtAuthorizationFilter,
     private val jwtAuthenticationEntryPoint: JwtAuthenticationEntryPoint,
-) : WebSecurityConfigurerAdapter() {
+) {
 
-    override fun configure(http: HttpSecurity) {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http.csrf().disable()
             .authorizeRequests()
             .antMatchers("/api/v1/auth/anonym").permitAll()
@@ -32,5 +32,6 @@ class SecurityConfig @Autowired constructor(
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter::class.java)
+        return http.build()
     }
 }
